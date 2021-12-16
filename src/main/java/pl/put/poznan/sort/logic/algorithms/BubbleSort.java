b99efbbda2c20c2225e32241793bcc89dbc9c750
Konrad Bankiewicz <kondziobank@gmail.com>
2021-12-16 23:40:29 +0100

    Fixed typos

--- src/main/java/pl/put/poznan/sort/logic/algorithms/BubbleSort.java
+++ src/main/java/pl/put/poznan/sort/logic/algorithms/BubbleSort.java
@@ -12,7 +12,7 @@ public class BubbleSort<T extends Comparable<T>> extends SortingAlgorithm<T> {
     public void sort(List<T> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size() - i - 1; j++) {
-                if (list.get(j).compareTo(list.get(j+1)) > 0)
+                if (list.get(j).compareTo(list.get(j + 1)) > 0)
                 {
                     Collections.swap(list, j, j + 1);
                 }
