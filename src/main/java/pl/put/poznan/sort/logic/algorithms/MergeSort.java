a9df08f2e80bde863f966a304bc48ceb86df24ac
qwercik <erykandrzejewski@gmail.com>
2021-12-17 00:47:00 +0100

    Implement MergeSort

---
+++ src/main/java/pl/put/poznan/sort/logic/algorithms/MergeSort.java
@@ -0,0 +1,54 @@
+package pl.put.poznan.sort.logic.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSort<T extends Comparable<T>> extends SortingAlgorithm<T> {
+    public MergeSort() {
+        super("merge");
+    }
+
+    @Override
+    public void sort(List<T> list) {
+        if (list.size() == 1) {
+            return;
+        }
+
+        List<T> left = new ArrayList<>(list.subList(0, list.size() / 2));
+        List<T> right = new ArrayList<>(list.subList(list.size() / 2, list.size()));
+
+        sort(left);
+        sort(right);
+        merge(left, right, list);
+    }
+
+    private void merge(List<T> left, List<T> right, List<T> result) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int resultIndex = 0;
+
+        while (leftIndex < left.size() && rightIndex < right.size()) {
+            if (left.get(leftIndex).compareTo(right.get(rightIndex)) <= 0) {
+                result.set(resultIndex, left.get(leftIndex));
+                leftIndex++;
+            } else {
+                result.set(resultIndex, right.get(rightIndex));
+                rightIndex++;
+            }
+
+            resultIndex++;
+        }
+
+        while (leftIndex < left.size()) {
+            result.set(resultIndex, left.get(leftIndex));
+            leftIndex++;
+            resultIndex++;
+        }
+
+        while (rightIndex < right.size()) {
+            result.set(resultIndex, right.get(rightIndex));
+            rightIndex++;
+            resultIndex++;
+        }
+    }
+}
