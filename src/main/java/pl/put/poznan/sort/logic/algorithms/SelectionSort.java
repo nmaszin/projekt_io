593317a5cc6d1bfe8ab59f915f980b1be64c3aaf
Paweł Błoch <pawblo28@gmail.com>
2021-12-17 13:38:02 +0100

    Implementacja SelectionSort

---
+++ src/main/java/pl/put/poznan/sort/logic/algorithms/SelectionSort.java
@@ -0,0 +1,21 @@
+package pl.put.poznan.sort.logic.algorithms;
+
+import java.util.Collections;
+import java.util.List;
+public class SelectionSort<T extends  Comparable<T>> extends SortingAlgorithm<T> {
+    public SelectionSort(){
+        super("selection");
+    }
+    @Override
+    public void sort(List<T> list){
+        for(int i=0;i<list.size(); i++){
+            int id=i;
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(j).compareTo(list.get(id))<0){
+                    id=j;
+                }
+            }
+            Collections.swap(list, id, i);
+        }
+    }
+}
