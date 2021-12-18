5bca9cbb6c4176989a860834227c2d2a87034f7a
Paweł Błoch <pawblo28@gmail.com>
2021-12-18 23:44:19 +0100

    implementacja SortableObject

---
+++ src/main/java/pl/put/poznan/sort/logic/SortableObject.java
@@ -0,0 +1,37 @@
+package pl.put.poznan.sort.logic;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+
+public class SortableData implements Comparable<SortableData> {
+    protected final JsonNode data;
+
+    public SortableData(JsonNode data) {
+        this.data = data;
+    }
+
+    public JsonNode getData() {
+        return this.data;
+    }
+
+    @Override
+    public int compareTo(SortableData other) {
+        if (this.data.isNumber() && other.data.isNumber()) {
+            Double left = this.data.asDouble();
+            Double right = other.data.asDouble();
+            return left.compareTo(right);
+        } else if (this.data.isTextual() && other.data.isTextual()) {
+            String left = this.data.asText();
+            String right = other.data.asText();
+            return left.compareTo(right);
+        } else {
+            throw new SortableDataInvalidDataTypeException("Trying to compare different types");
+        }
+    }
+}
+
+class SortableDataInvalidDataTypeException extends RuntimeException {
+    public SortableDataInvalidDataTypeException(String message) {
+        super(message);
+    }
+}
