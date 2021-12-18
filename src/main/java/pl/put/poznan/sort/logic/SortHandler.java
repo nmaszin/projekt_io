6e9856644cbfdd678ed676a9ed8e55974f06a495
qwercik <erykandrzejewski@gmail.com>
2021-12-18 22:40:55 +0100

    Example implementation of SortableData and SortableObject

--- src/main/java/pl/put/poznan/sort/logic/SortHandler.java
+++ src/main/java/pl/put/poznan/sort/logic/SortHandler.java
@@ -3,7 +3,7 @@ package pl.put.poznan.sort.logic;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pl.put.poznan.sort.logic.SortTask;
+import pl.put.poznan.sort.logic.algorithms.BubbleSort;
 import pl.put.poznan.sort.rest.SortController;
 
 import java.util.ArrayList;
@@ -13,25 +13,37 @@ import java.util.stream.Collectors;
 public class SortHandler {
     private static final Logger logger = LoggerFactory.getLogger(SortController.class);
 
-    private SortTask task;
-    private List<SortableData> dataToSort;
+    private final SortTask task;
+    private final List<SortableData> dataToSort;
 
-    private List<SortableData> extractDataListFromJson(List<JsonNode> jsonNode) {
+    private List<SortableData> extractDataListFromJson(List<JsonNode> jsonNode, String key) {
         return jsonNode.stream()
-            .map(SortableData::new)
+            .map(e -> new SortableData(e, key))
+            .collect(Collectors.toList());
+    }
+
+    private List<JsonNode> extractJsonListFromSortableData(List<SortableData> sortableData) {
+        return sortableData.stream()
+            .map(SortableData::getData)
             .collect(Collectors.toList());
     }
 
     public SortHandler(SortTask task) {
         this.task = task;
-        this.dataToSort = extractDataListFromJson(task.getData());
+        this.dataToSort = extractDataListFromJson(task.getData(), task.getKey());
     }
 
     public SortResult run() {
         List<AlgorithmResult> algorithmsResults = new ArrayList<>();
         for (String algorithmName : this.task.getAlgorithms()) {
+            List<SortableData> dataCopy = new ArrayList<>(this.dataToSort);
+            BubbleSort<SortableData> sorter = new BubbleSort<SortableData>();
+            sorter.sort(dataCopy);
+
+            List<JsonNode> sortedDataAsJson = extractJsonListFromSortableData(dataCopy);
+
             AlgorithmResult result = new AlgorithmResult(
-                algorithmName, this.task.getData(), 0.112);
+                algorithmName, sortedDataAsJson, 0.112);
             algorithmsResults.add(result);
         }
 
--- src/main/java/pl/put/poznan/sort/logic/SortableData.java
+++ src/main/java/pl/put/poznan/sort/logic/SortableData.java
@@ -2,11 +2,14 @@ package pl.put.poznan.sort.logic;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
-public class SortableData implements Comparable<SortableData> {
-    private final JsonNode data;
 
-    public SortableData(JsonNode data) {
+public class SortableData implements Comparable<SortableData> {
+    protected final JsonNode data;
+    private final String key;
+
+    public SortableData(JsonNode data, String key) {
         this.data = data;
+        this.key = key;
     }
 
     public JsonNode getData() {
@@ -14,8 +17,27 @@ public class SortableData implements Comparable<SortableData> {
     }
 
     @Override
-    public int compareTo(SortableData sortableData) {
-        // TODO: implement
-        return 0;
+    public int compareTo(SortableData other) {
+        if (this.data.isNumber() && other.data.isNumber()) {
+            Double left = this.data.asDouble();
+            Double right = other.data.asDouble();
+            return left.compareTo(right);
+        } else if (this.data.isTextual() && other.data.isTextual()) {
+            String left = this.data.asText();
+            String right = other.data.asText();
+            return left.compareTo(right);
+        } else if (this.data.isObject() && other.data.isObject()) {
+            SortableObject left = new SortableObject(this.data, this.key);
+            SortableObject right = new SortableObject(other.data, this.key);
+            return left.compareTo(right);
+        } else {
+            throw new SortableDataInvalidDataTypeException("Trying to compare different types");
+        }
+    }
+}
+
+class SortableDataInvalidDataTypeException extends RuntimeException {
+    public SortableDataInvalidDataTypeException(String message) {
+        super(message);
     }
 }
