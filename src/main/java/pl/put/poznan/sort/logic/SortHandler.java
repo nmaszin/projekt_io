2d1ff725562476a35f235d550ef26029167816fd
linek150 <linek11@vp.pl>
2021-12-21 09:10:14 +0100

    Sort handler implementation

--- src/main/java/pl/put/poznan/sort/logic/SortHandler.java
+++ src/main/java/pl/put/poznan/sort/logic/SortHandler.java
@@ -3,11 +3,13 @@ package pl.put.poznan.sort.logic;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pl.put.poznan.sort.logic.algorithms.BubbleSort;
+import pl.put.poznan.sort.logic.algorithms.*;
 import pl.put.poznan.sort.rest.SortController;
 
+import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public class SortHandler {
@@ -37,16 +39,45 @@ public class SortHandler {
         List<AlgorithmResult> algorithmsResults = new ArrayList<>();
         for (String algorithmName : this.task.getAlgorithms()) {
             List<SortableData> dataCopy = new ArrayList<>(this.dataToSort);
-            BubbleSort<SortableData> sorter = new BubbleSort<>();
-            sorter.sort(dataCopy);
-
+            double time=sortDataWith(algorithmName, dataCopy);
             List<JsonNode> sortedDataAsJson = extractJsonListFromSortableData(dataCopy);
             AlgorithmResult result = new AlgorithmResult(
-                    algorithmName, sortedDataAsJson, 0.112);
+                    algorithmName, sortedDataAsJson, time);
             algorithmsResults.add(result);
         }
-
         return new SortResult(algorithmsResults);
+    }
+
+    public double sortDataWith(String algorithmName, List<SortableData> data){
+        SortingAlgorithm<SortableData> sorter = getAlgorithmByName(algorithmName);
+        long startTime = System.currentTimeMillis();
+        sorter.sort(data);
+        return ((double)System.currentTimeMillis() - startTime)/1000;
+    }
+
+    private SortingAlgorithm<SortableData> getAlgorithmByName(String name){
+        SortingAlgorithm<SortableData> sorter;
+        switch(name){
+            case "bubble":
+                return new BubbleSort<>();
+
+            case "gnome":
+                return new GnomeSort<>();
+
+            case "insertion":
+                return new InsertionSort<>();
+
+            case "merge":
+                return new MergeSort<>();
+
+            case "quick":
+                return new QuickSort<>();
+
+            case "selection":
+                return new SelectionSort<>();
+            default:
+                return new QuickSort<>();
+        }
     }
 }
 
