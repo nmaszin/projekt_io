539ba6f5c7b2a6f56747ed298bef4026d58b343a
qwercik <erykandrzejewski@gmail.com>
2022-01-18 17:38:39 +0100

    Refactor

--- src/test/java/pl/put/poznan/sort/logic/SortableObjectTest.java
+++ src/test/java/pl/put/poznan/sort/logic/SortableObjectTest.java
@@ -10,65 +10,67 @@ import com.fasterxml.jackson.databind.JsonNode;
 import pl.put.poznan.sort.logic.exceptions.SortableObjectKeyNotExistsException;
 
 public class SortableObjectTest {
-
     private JsonNode SimpleTestNode;
     private JsonNode CompositeTestNode;
+
     @BeforeEach
     void init() {
-        String SimpleJsonString="{\"a\":\"2\", \"b\":\"3\"}";
-        String CompositeJsonString="{\"a\":{\"b\":\"2\"}}";
-        ObjectMapper mapper=new ObjectMapper();
+        String SimpleJsonString = "{\"a\":\"2\", \"b\":\"3\"}";
+        String CompositeJsonString = "{\"a\":{\"b\":\"2\"}}";
+        ObjectMapper mapper = new ObjectMapper();
         SimpleTestNode = null;
         CompositeTestNode = null;
+
         try {
-            SimpleTestNode=mapper.readTree(SimpleJsonString);
+            SimpleTestNode = mapper.readTree(SimpleJsonString);
+            CompositeTestNode = mapper.readTree(CompositeJsonString);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        try {
-            CompositeTestNode=mapper.readTree(CompositeJsonString);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-
     }
+
     @Test
      void compareToTest() {
-
-        SortableObject Soa=new SortableObject(SimpleTestNode,"a");
-        SortableObject Sob=new SortableObject(SimpleTestNode,"b");
-        assertEquals(0,Soa.compareTo(Soa));
-        assertEquals(-1,Soa.compareTo(Sob));
+        SortableObject Soa = new SortableObject(SimpleTestNode, "a");
+        SortableObject Sob = new SortableObject(SimpleTestNode, "b");
+        assertEquals(0, Soa.compareTo(Soa));
+        assertEquals(-1, Soa.compareTo(Sob));
         assertEquals(1, Sob.compareTo(Soa));
     }
-    @Test
-    void SortableObjectTest(){
-        SortableObject So=new SortableObject(SimpleTestNode,"a");
-    }
-    @Test
-    void NullKeyTest(){
 
-        SortableObjectKeyNotExistsException thrown= Assertions.assertThrows(SortableObjectKeyNotExistsException.class,
-                ()-> {SortableObject So=new SortableObject(SimpleTestNode,null);},"good");
-        assertEquals(pl.put.poznan.sort.logic.exceptions.SortableObjectKeyNotExistsException.class,SortableObjectKeyNotExistsException.class);
-    }
     @Test
-    void KeyNotExistTest(){
-        SortableObjectKeyNotExistsException thrown= Assertions.assertThrows(SortableObjectKeyNotExistsException.class,
-                ()-> {SortableObject So=new SortableObject(CompositeTestNode,"d");},"good");
-        assertEquals(pl.put.poznan.sort.logic.exceptions.SortableObjectKeyNotExistsException.class,SortableObjectKeyNotExistsException.class);
-    }
-    @Test
-    void CompositeKeyTest(){
-        SortableObject Soa=new SortableObject(CompositeTestNode,"a.b");
+    void SortableObjectTest() {
+        SortableObject So = new SortableObject(SimpleTestNode, "a");
     }
 
+    @Test
+    void NullKeyTest() {
+        SortableObjectKeyNotExistsException thrown = Assertions.assertThrows(
+			SortableObjectKeyNotExistsException.class,
+            ()-> {
+				SortableObject So = new SortableObject(SimpleTestNode, null);
+			},
+			"good"
+		);
+        assertEquals(pl.put.poznan.sort.logic.exceptions.SortableObjectKeyNotExistsException.class, SortableObjectKeyNotExistsException.class);
+    }
 
+    @Test
+    void KeyNotExistTest() {
+        SortableObjectKeyNotExistsException thrown = Assertions.assertThrows(
+			SortableObjectKeyNotExistsException.class,
+        	() -> {
+				SortableObject So = new SortableObject(CompositeTestNode, "d");
+			},
+			"good"
+		);
 
+        assertEquals(pl.put.poznan.sort.logic.exceptions.SortableObjectKeyNotExistsException.class, SortableObjectKeyNotExistsException.class);
+    }
 
-
-
-
-
+    @Test
+    void CompositeKeyTest() {
+        SortableObject Soa = new SortableObject(CompositeTestNode, "a.b");
+    }
 }
+
